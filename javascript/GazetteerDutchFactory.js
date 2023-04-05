c71f35da9b54f4178d85f42f2ef150e25624b295
Juan Luis Rodríguez <juanluisrp@gmail.com>
2023-04-05 16:05:44 +0200

    Update location service to new PDOK URLs
    The Gazetteer service ofered by PDOK has new URLs. This commit updates
    the URLs to the new ones.

--- javascript/GazetteerDutchFactory.js
+++ javascript/GazetteerDutchFactory.js
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2001-2016 Food and Agriculture Organization of the
+ * Copyright (C) 2001-2023 Food and Agriculture Organization of the
  * United Nations (FAO-UN), United Nations World Food Programme (WFP)
  * and United Nations Environment Programme (UNEP)
  *
@@ -21,68 +21,73 @@
  * Rome - Italy. email: geonetwork@osgeo.org
  */
 
-(function() {
+(function () {
   goog.provide('gn_dutch_gazetteer_factory');
 
   var module = angular.module('gn_dutch_gazetteer_factory', []);
 
-  module.provider('gnDutchGazetteer', function() {
+  module.constant('serviceUrls', {
+    'lookupServiceUrl': 'https://api.pdok.nl/bzk/locatieserver/search/v3_1/lookup',
+    'suggestServiceUrl': 'https://api.pdok.nl/bzk/locatieserver/search/v3_1/suggest'
+  });
+
+  module.provider("gnDutchGazetteer", ['serviceUrls', function (serviceUrls) {
     return {
-      $get : [
+      $get: [
         '$http',
         'gnGlobalSettings',
         'gnViewerSettings',
         'gnGetCoordinate',
-        function($http, gnGlobalSettings, gnViewerSettings, gnGetCoordinate) {
-          var zoomTo = function(extent, map, zoom) {
+        function ($http, gnGlobalSettings, gnViewerSettings, gnGetCoordinate) {
+          var zoomTo = function (extent, map, zoom) {
             // fit geometry
             map.getView().fit(extent, map.getSize());
             // zoom in
             if (zoom) {
               map.getView().setZoom(zoom);
-            }          
+            }
           }
           return {
-            onClick: function(scope, loc, map) {
-            	// get the details from the lookup service
-              var url = 'https://geodata.nationaalgeoregister.nl/locatieserver/lookup';
-              $http.get(url, {
-                params: {
-                  id: loc.id
-                }
-              }).
-              success(function(response) {
-
-                if (response.response.numFound > 0) {
-
-                	var dataPoint = response.response.docs[0].centroide_ll;
-                	var mapProjection = map.getView().getProjection().getCode();
-                  // turn it into a geometry and convert
-                  var geom = new ol.format.WKT().readGeometry(dataPoint).transform('EPSG:4326', mapProjection);
-                  // zoom to the coordinates
-                  // 
-                  // zoom depends on type
-                  var zoom = 10;
-                  var type = response.response.docs[0].type;
-
-                  if (type == 'gemeente') {
-                    zoom = 8;
-                  } else if (type == 'woonplaats') {
-                    zoom = 9;
-                  } else if (type == 'weg' || type == 'postcode') {
-                    zoom = 12;
-                  } else if (type == 'adres' || type == 'hectometerpaal' || type == 'perceel') {
-                    zoom = 13;
+            onClick: function (scope, loc, map) {
+              // get the details from the lookup service
+              $http
+                .get(serviceUrls.lookupServiceUrl, {
+                  params: {
+                    id: loc.id
                   }
+                })
+                .then(function (response) {
+                  if (response.data.response.numFound > 0) {
+                    var dataPoint = response.data.response.docs[0].centroide_ll;
+                    var mapProjection = map.getView().getProjection().getCode();
+                    // turn it into a geometry and convert
+                    var geom = new ol.format.WKT()
+                      .readGeometry(dataPoint)
+                      .transform("EPSG:4326", mapProjection);
+                    // zoom to the coordinates
+                    //
+                    // zoom depends on type
+                    var zoom = 10;
+                    var type = response.data.response.docs[0].type;
 
-                  // fit the geometry and zoom
-                  zoomTo(geom, map, zoom);
-                  scope.query = loc.name;
-                  scope.collapsed = true;
-                }
-              });
+                    if (type == 'gemeente') {
+                      zoom = 8;
+                    } else if (type == 'woonplaats') {
+                      zoom = 9;
+                    } else if (type == 'weg' || type == 'postcode') {
+                      zoom = 12;
+                    } else if (type == 'adres' || type == 'hectometerpaal' || type == 'perceel') {
+                      zoom = 13;
+                    }
+
+                    // fit the geometry and zoom
+                    zoomTo(geom, map, zoom);
+                    scope.query = loc.name;
+                    scope.collapsed = true;
+                  }
+                });
             },
-            search: function(scope, loc, query) {
+            search: function (scope, loc, query) {
               if (query.length < 3) return;
 
               var coord = gnGetCoordinate(
@@ -96,48 +101,50 @@
                   view.setCenter(center);
                 }
                 moveTo(scope.map, 5, ol.proj.transform(coord,
-                    'EPSG:4326', 'EPSG:28992'));
+                  'EPSG:4326', 'EPSG:28992'));
                 return;
               }
-              var formatter = function(loc) {
+              var formatter = function (loc) {
                 var props = [];
                 ['toponymName', 'adminName1', 'countryName'].
-                    forEach(function(p) {
-                      if (loc[p]) { props.push(loc[p]); }
-                    });
+                  forEach(function (p) {
+                    if (loc[p]) { props.push(loc[p]); }
+                  });
                 return (props.length == 0) ? '' : '—' + props.join(', ');
               };
-              var url = 'https://geodata.nationaalgeoregister.nl/locatieserver/suggest';
-              $http.get(url, {
-                params: {
-                  q: query
-                }
-              }).
-              success(function(response) {
-                // array for the search results
-                scope.results = [];
-                // no results, just stop, don't build the dropdown
-                var numResults = response.response.numFound;
+              $http
+                .get(serviceUrls.suggestServiceUrl, {
+                  params: {
+                    q: query
+                  }
+                })
+                .then(function (response) {
+                  // array for the search results
+                  scope.results = [];
 
-                if (numResults == 0) {
-                  return;
-                }
-                // get the results
-                $features = response.response.docs;
-                // loop through the results
-                $.each($features, function(i, item) {
-                  // create the result
-                  scope.results.push({
-                    id: item.id,
-                    name: item.weergavenaam,
-                    type: item.type,
-                    score: item.score
+                  // no results, just stop, don't build the dropdown
+                  var numResults = response.data.response.numFound;
+
+                  if (numResults == 0) {
+                    return;
+                  }
+                  // get the results
+                  $features = response.data.response.docs;
+                  // loop through the results
+                  $.each($features, function (i, item) {
+                    // create the result
+                    scope.results.push({
+                      id: item.id,
+                      name: item.weergavenaam,
+                      type: item.type,
+                      score: item.score
+                    });
                   });
                 });
-              });
             }
-          }
-        }]
-    }
-  });
+          };
+        }
+      ]
+    };
+  }]);
 })();
