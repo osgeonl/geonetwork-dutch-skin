42fb040557d4bac2fe65f5005bac02c8b88632e2
Jose García <josegar74@gmail.com>
2024-04-24 12:04:14 +0200

    Update result template with changes from https://github.com/geonetwork/core-geonetwork/pull/7376

--- module.js
+++ module.js
@@ -174,9 +174,11 @@
     "$scope",
     "gnRelatedResources",
     function ($scope, gnRelatedResources) {
-      $scope.resultTemplate =
-        "../../catalog/components/" +
-        "search/resultsview/partials/viewtemplates/grid4maps.html";
+      $scope.resultTemplate = {
+        tplUrl:
+          "../../catalog/components/" +
+          "search/resultsview/partials/viewtemplates/grid4maps.html"
+      };
       $scope.searchObj = {
         permalink: false,
         internal: true,
@@ -275,7 +277,7 @@
       $scope.activeTab = "/home";
       $scope.formatter = gnGlobalSettings.gnCfg.mods.search.formatter;
       $scope.listOfResultTemplate = gnGlobalSettings.gnCfg.mods.search.resultViewTpls;
-      $scope.resultTemplate = gnSearchSettings.resultTemplate;
+      $scope.resultTemplate = gnGlobalSettings.getDefaultResultTemplate();
       $scope.dateType = "metadata";
       /* Default advanced search form template */
       $scope.advancedSearchTemplate =
